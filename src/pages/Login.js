4634af18ebe89f16d9f4ecf4f57ecc592a109511
Robert Laws <roblaws@me.com>
2021-12-30 20:56:52 +0300

    update form html and css

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -16,24 +16,28 @@ export const Login = () => {
     <div>
       <form className='app-form' onSubmit={handleSubmit}>
         <h2>Login</h2>
-        <label>
-          <span>email</span>
-          <input
-            type='email'
-            name='email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </label>
-        <label>
-          <span>password</span>
-          <input
-            type='password'
-            name='password'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </label>
+        <div className='fields'>
+          <label>
+            <span>email</span>
+            <input
+              type='email'
+              name='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </label>
+        </div>
+        <div className='fields'>
+          <label>
+            <span>password</span>
+            <input
+              type='password'
+              name='password'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </label>
+        </div>
         <div className='form-submit'>
           <button className={`${isPending && 'button-loading'}`}>
             <span className='button-text'>Login</span>
