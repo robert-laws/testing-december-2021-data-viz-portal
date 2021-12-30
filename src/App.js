39375b7f5faa1caf017713ad2f5e5a4d429b3496
Robert Laws <roblaws@me.com>
2021-12-30 11:56:19 +0300

    add basic site routing

--- src/App.js
+++ src/App.js
@@ -4,9 +4,19 @@ import { Home, Login, Signup, Profile, KnowledgeBase, NotFound } from './pages';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>App</h1>
-    </div>
+    <Router>
+      <div className='App'>
+        <h1>App</h1>
+      </div>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/knowledge-base' element={<KnowledgeBase />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
+    </Router>
   );
 }
 
